aedb7ff2c341be10589b02272820e73e55459b37
LUCAS DE SOUSA SANTOS <lucas.ssantos189@senacsp.edu.br>
2024-02-21 22:20:12 -0300

    projeto

---
+++ JS/interacoes.js
@@ -0,0 +1,12 @@
+// hamburger
+
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+
+    
+}
+   
