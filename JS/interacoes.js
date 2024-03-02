d290fdc9dd25834eab40914e7405aca7a1c63dd5
lucas sousa <lucassousa19911@outlook.com>
2024-03-01 21:35:31 -0300

    salvar projeto

--- JS/interacoes.js
+++ JS/interacoes.js
@@ -1,12 +1,11 @@
-// hamburger
+// Hamburger
 
 function clickMenu(){
     if(itens.style.display == 'block'){
         itens.style.display = 'none'
     } else{
         itens.style.display = 'block'
-    }
-
-    
+    } 
+    // Se eu clicar (abre)
+    // caso contrario (fecha)
 }
-   
